709329adb1e482f9a8474f5e611bdd7a48e406c1
HYEMIN JO <gkdlt7373@gmail.com>
2023-06-15 21:58:29 +0900

    ddd

--- js/main.js
+++ js/main.js
@@ -69,12 +69,11 @@ $.datepicker.setDefaults({
   yearSuffix: "년",
 });
 
-
 let swiper = new Swiper(".challenge_swiper", {
   pagination: {
     el: ".swiper-pagination",
     dynamicBullets: false,
-  }
+  },
 });
 
 //로컬 저장된 값 없는 경우 mainBox display:flex
@@ -127,18 +126,21 @@ const save = () => {
 
 //datepicker 날짜 로컬스토리지 저장
 let num = 0;
+
 const localSave = () => {
   const challengeObj = {
     seq: ++num,
     startDate: startDate,
     endDate: endDate,
     amount: selectAmount.value,
+    remainingAmount: selectAmount.value,
+    userSpendList: "",
   };
-  if(startDate!=null){//입력한 값이 있는 경우만 local 저장
+  if (startDate != null) {
+    //입력한 값이 있는 경우만 local 저장
     challengeArray.push(challengeObj);
     save(); //로컬스토리지에 challengeArray배열 저장
   }
-
 };
 
 //기간, 금액 입력 후 최종 확인 버튼
@@ -169,78 +171,87 @@ spendBtn.addEventListener("click", () => {
 
 let inputAmount; //최초 금액
 let usedAmount = 0; //총 사용 금액
-let remainingAmount = selectAmount.value; //잔여 금액
 let curAmount;
 let slideActive;
 
 //전체 금액에서 잔여 금액 계산 함수
 const remainingAmountCalc = () => {
-  console.log('전체배열',challengeArray);
-  slideActive = document.querySelector('.swiper-slide-active');
-  console.log('클릭 시 active 번호',slideActive.dataset.seq);
+  slideActive = document.querySelector(".swiper-slide-active");
+  let seqActive = slideActive.dataset.seq; //현재 active slide의 seq 번호
+  let spendDate = new Date();
 
-  challengeArray.forEach((el)=>{
-    if(slideActive.dataset.seq == el.seq){
-      console.log('같음');
+  challengeArray.forEach((el) => {
+    if (seqActive == el.seq) {
+      let amountValue = el.amount.replace(/,/g, ""); //콤마 제거
+      let inputSpendAmount = spendAmount.value;
+      let spendAmountValue = inputSpendAmount.replace(/,/g, ""); //숫자
+
+      const spendItem = {
+        spendItem: spendCont.value,
+        spendAmount: spendAmountValue,
+        spendDate: spendDate,
+      };
+      if (el.userSpendList == "") {
+        el.userSpendList = [spendItem];
+      } else {
+        el.userSpendList = [...el.userSpendList, spendItem];
+      }
+      let spendAmountArr = [
+        //spendAmount 값만 출력해서 새로운 배열 생성
+        ...new Set(el.userSpendList.map((item) => item.spendAmount)),
+      ];
+
+      let spendAmountArrNum = spendAmountArr.map(Number);
+      let spendAmountSum = spendAmountArrNum.reduce((a, b) => a + b); //사용 금액 합계
+
+      el.remainingAmount = amountValue - spendAmountSum; //잔여 금액
+
+      //사용퍼센트 구해서 스타일 적용
+      let remainingPercentage = (el.remainingAmount / amountValue) * 100; //잔여금액 퍼센트
+      let totalSpendPercentage = 100 - remainingPercentage; //총 사용금액 퍼센트
+      let strokeDashOffsetPercentage = (totalSpendPercentage / 100) * 0.9 + 0.1; //사용퍼센트를 0.1~1까지 변경
+      let strokeDashOffsetCalc = `calc(720 - (720 * ${
+        1 - strokeDashOffsetPercentage
+      }))`;
+      //calc 값 구함
+
+      el.offset = strokeDashOffsetCalc;
+      save();
+      barOffset();
+      curAmount = document.querySelector(".swiper-slide-active .cur_amount");
+      curAmount.innerHTML = `${el.remainingAmount.toLocaleString()}원`;
+      //spendCont.value = ""; //사용내역 초기화
     }
-  })
+  });
+};
 
+//로컬에서 offset 값 가져와서 bar.style 조정
+const barOffset = () => {
+  let bar = document.querySelector(".swiper-slide-active .bar");
+  let barOffsetValue = JSON.parse(localStorage.getItem("challengeData"));
 
-  inputAmount = selectAmount.value; //전체 금액
-  let inputAmountValue = inputAmount.replace(/,/g, ""); //숫자
-
-  let inputSpendAmount = spendAmount.value; //입력한 사용금액 계산 위해 숫자로 변환 but 콤마 포함되어있음
-  let spendAmountValue = inputSpendAmount.replace(/,/g, ""); //숫자
-
-  usedAmount += parseInt(spendAmountValue); //사용자 입력 금액 -> 사용 금액에 더하기
-  remainingAmount = inputAmountValue - usedAmount; //잔여 금액
-  console.log("remainingAmount", remainingAmount);
-  console.log("inputAmountValue", inputAmountValue);
-
-  let remainingPercentage = (remainingAmount / inputAmountValue) * 100; //잔여금액 퍼센트
-  let totalSpendPercentage = 100 - remainingPercentage; //총 사용금액 퍼센트
-
-  let strokeDashOffsetPercentage = (totalSpendPercentage / 100) * 0.9 + 0.1; //사용퍼센트를 0.1~1까지 변경
-  let strokeDashOffsetCalc = `calc(720 - (720 * ${
-    1 - strokeDashOffsetPercentage
-  }))`; //calc 값 구함
-
-  let bar = document.querySelector(".bar");
-  bar.style.strokeDashoffset = strokeDashOffsetCalc;
-
-  curAmount.innerHTML = `${remainingAmount.toLocaleString()}원`;
-  console.log("curAmount", curAmount);
-
-  spendAmount.value = "";
-  spendCont.value = ""; //사용내역 초기화
+  barOffsetValue.forEach((el) => {
+    console.log("barrrrr", bar);
+    bar.style.strokeDashoffset = el.offset;
+  });
+  console.log("슬라이드 변경");
 };
 
 //사용내역 추가 클릭 시 원래 금액에서 입력 금액만큼 빠져야함
 spendAmountBtn.addEventListener("click", () => {
   challengeSpend.classList.remove("spend_on");
-  //해당 슬라이드가 swiper-active일때 seq번호를 비교해서 해당 배열에서만 금액 빠지게 swiper-slide-active
-
-
   remainingAmountCalc();
 });
 
 //챌린지 생성 함수
 const challengeWrap = document.querySelector(".challenge_wrap");
 
-//챌린지 데이터들 담을 변수 생성
-let challengeDates = [];
-
 const createChallenge = () => {
-  challengeWrap.innerHTML = '';
-
+  challengeWrap.innerHTML = "";
   localSave();
-  console.log("저장함 ",challengeArray);
-  //로컬스토리지에서 값 빼오기
-  console.log('빼옴',challengeArray);
-
   let seqNum = 0;
 
-  challengeArray.forEach((el)=> {
+  challengeArray.forEach((el) => {
     let today = new Date();
     let endDday = new Date(el.endDate);
     let endGap = endDday.getTime() - today.getTime();
@@ -249,9 +260,9 @@ const createChallenge = () => {
     let challengeInner = document.createElement("div");
     challengeInner.classList.add("challenge_inner", "swiper-slide");
     challengeInner.setAttribute("data-seq", ++seqNum);
-  
+
     challengeInner.innerHTML = `
-    <div class="challenge_info">
+    <div class="challenge_info" id=${seqNum}>
       <p class="info_date">${el.startDate} ~ ${el.endDate}</p>
       <p class="info_dday">챌린지 종료까지<span class="calc_dday">${result.toString()}일</span>남았어요</p>
       <div class="progress">진행중</div>
@@ -261,7 +272,7 @@ const createChallenge = () => {
         <div class="circle_outer">
           <div class="circle_inner">
             <div class="circle_number">
-              <div class="cur_amount">${el.amount}원</div>
+              <div class="cur_amount">${el.remainingAmount}원</div>
               <div class="pre_amount">￦${el.amount}</div>
             </div>
           </div>
@@ -278,28 +289,29 @@ const createChallenge = () => {
       </div>
     </div>
     `;
-
     challengeWrap.append(challengeInner); //append가 아닌 appendChild로 하나씩 새로 추가
-    console.log()
 
-    curAmount = document.querySelector(".cur_amount"); //잔여 예산 */
-  
     $("#datepicker_start").datepicker("setDate", "");
     $("#datepicker_end").datepicker("setDate", "");
 
-
-   swiper.update(); // 슬라이드 요소 업데이트
-  })
+    swiper.update(); // 슬라이드 요소 업데이트
+  });
 };
 
-window.addEventListener("load",()=>{
-  if(challengeArray.length > 0) {
+//페이지 새로 고침 시
+window.addEventListener("load", () => {
+  if (challengeArray.length > 0) {
     mainChallenge.style.display = "block";
     mainBox.style.display = "none";
-      createChallenge();
-    
+    createChallenge();
+    barOffset();
   } else {
     mainChallenge.style.display = "none";
     mainBox.style.display = "flex";
+    barOffset();
   }
-})
+});
+
+swiper.on("slideChange", () => {
+  barOffset();
+});
